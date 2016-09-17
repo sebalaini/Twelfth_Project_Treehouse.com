d59f99219e9891079b333de73a08e518eaa10071
Sebastiano <sebastiano@Sebas-MacBook-Pro.local>
2016-09-17 15:11:29 +0200

    portfolio layout

--- js/main.js
+++ js/main.js
@@ -1,122 +1,98 @@
 $(document).ready(function(){
 
 $("#repository1").click(function() {
-	event.preventDefault();
 	window.open("https://github.com/sebalaini/First_Project_Treehouse.com");
 });
 
 $("#livepage1").click(function() {
-	event.preventDefault();
 	window.open("http://sebalaini.github.io/First_Project_Treehouse.com/");
 });
 
 $("#repository2").click(function() {
-	event.preventDefault();
 	window.open("https://github.com/sebalaini/Second_Project_Treehouse.com");
 });
 
 $("#livepage2").click(function() {
-	event.preventDefault();
 	window.open("http://sebalaini.github.io/Second_Project_Treehouse.com/");
 });
 
 $("#repository3").click(function() {
-	event.preventDefault();
 	window.open("https://github.com/sebalaini/Third_Project_Treehouse.com");
 });
 
 $("#livepage3").click(function() {
-	event.preventDefault();
 	window.open("http://sebalaini.github.io/Third_Project_Treehouse.com/");
 });
 
 $("#repository4").click(function() {
-	event.preventDefault();
 	window.open("https://github.com/sebalaini/Forth_Project_Treehouse.com");
 });
 
 $("#livepage4").click(function() {
-	event.preventDefault();
 	window.open("http://sebalaini.github.io/Forth_Project_Treehouse.com/");
 });
 
 $("#repository5").click(function() {
-	event.preventDefault();
 	window.open("https://github.com/sebalaini/Fifth_Project_Treehouse.com");
 });
 
 $("#livepage5").click(function() {
-	event.preventDefault();
 	window.open("http://sebalaini.github.io/Fifth_Project_Treehouse.com/");
 });
 
 $("#repository6").click(function() {
-	event.preventDefault();
 	window.open("https://github.com/sebalaini/Sixth_Project_Treehouse.com");
 });
 
 $("#livepage6").click(function() {
-	event.preventDefault();
 	window.open("http://sebalaini.github.io/Sixth_Project_Treehouse.com/");
 });
 
 $("#repository7").click(function() {
-	event.preventDefault();
 	window.open("https://github.com/sebalaini/Seventh_Project_Treehouse.com");
 });
 
 $("#livepage7").click(function() {
-	event.preventDefault();
 	window.open("http://sebalaini.github.io/Seventh_Project_Treehouse.com/");
 });
 
 $("#repository8").click(function() {
-	event.preventDefault();
 	window.open("https://github.com/sebalaini/Eighth_Project_Treehouse.com");
 });
 
 $("#livepage8").click(function() {
-	event.preventDefault();
 	window.open("http://sebalaini.github.io/Eighth_Project_Treehouse.com/");
 });
 
 $("#repository9").click(function() {
-	event.preventDefault();
 	window.open("https://github.com/sebalaini/Ninth_Project_Treehouse.com");
 });
 
 $("#livepage9").click(function() {
-	event.preventDefault();
 	window.open("http://sebalaini.github.io/Ninth_Project_Treehouse.com/");
 });
 
 $("#repository10").click(function() {
-	event.preventDefault();
 	window.open("https://github.com/sebalaini/Tenth_Project_Treehouse.com");
 });
 
 $("#livepage10").click(function() {
-	event.preventDefault();
 	window.open("http://sebalaini.github.io/Tenth_Project_Treehouse.com/");
 });
 
 $("#repository11").click(function() {
-	event.preventDefault();
 	window.open("https://github.com/sebalaini/Eleventh_Project_Treehouse.com");
 });
 
 $("#livepage11").click(function() {
-	event.preventDefault();
 	window.open("http://sebalaini.github.io/Eleventh_Project_Treehouse.com/");
 });
 
 $("#repository12").click(function() {
-	event.preventDefault();
 	window.open("https://github.com/sebalaini/Twelfth_Project_Treehouse.com");
 });
 
 $("#livepage12").click(function() {
-	event.preventDefault();
 	window.open("http://sebalaini.github.io/Twelfth_Project_Treehouse.com/");
 });
 
