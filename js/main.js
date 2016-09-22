c921b7a98ed802bfbe10c34acc99fa70da77d6c0
Sebastiano <sebastiano@Sebas-MacBook-Pro.local>
2016-09-22 21:52:25 +0200

    nav change color

--- js/main.js
+++ js/main.js
@@ -8,24 +8,32 @@ MENU BTN
 		$( ".nav" ).toggle();
 	});
 
-/* var contentSections = $('.section');
+
+
+/***************
+A NAV CHANGE COLOR
+***************/
+
+var contentSections = $('.section');
 var navigationItems = $('.nav a');
 
-	updateNavigation();
-	$(window).on('scroll', function(){
-		updateNavigation();
-	});
+updateNavigation();
+$(window).on('scroll', function() {
+  updateNavigation();
+});
 
-	function updateNavigation() {
-		contentSections.each(function(){
-			$this = $(this);
-			var activeSection = $('.nav a[href="#'+$this.attr("class")+'"]');
-			if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
-				navigationItems.eq(activeSection).addClass('selected');
-			}else {
-				navigationItems.eq(activeSection).removeClass('selected');
-			}
-		});
-	} */ 
+function updateNavigation() {
+  
+  contentSections.each(function() {
+    $this = $(this);
+    var activeSection = $('.nav a[href="#' + $this.attr("id") + '"]');
+    
+    if (($this.offset().top - $(window).height() / 2 < $(window).scrollTop()) && ($this.offset().top + $this.height() - $(window).height() / 2 > $(window).scrollTop())) {
+      activeSection.addClass('selected');
+    } else {
+      activeSection.removeClass('selected');
+    }
+  });
+}
 
 });
